e9501c8686b0cb14d6b723e0fdef20bf32d97ab6
Ahmet Can GÜNAY <ahmetcangunay@outlook.com>
2021-01-20 23:50:46 +0300

    20 Ocak Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> {"Engin","Murat","Halil" };
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("Ahmet");
+            Console.WriteLine(isimler2[3]);
+            
+        }
+    }
+}
